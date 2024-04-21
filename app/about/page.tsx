30119392de646003d29c47c0f274dd80ad33e503
budhi <sapiderman.lumpat@gmail.com>
2024-04-21 21:46:40 +0700

    add dojo schedule

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,17 +8,16 @@ export default function Dojo() {
 
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="mb-1 text-2xl font-bold"> About Tenkei Aikidojo </h1>
-        <p>
+        <h1 className="mb-1 text-5xl font-bold"> About Tenkei Aikidojo </h1>
+        <p className="text-justify">
           Tenkei Aikidojo adalah kelompok dojo-dojo yang berada di bawah bimbingan Sensei Eka Machdi Ramdani (4th Dan), yang terdaftar langsung sebagai anggota Aikikai Hombu dojo Jepang. Dojo-dojo yang berada dalam Aikikai menekankan latihan Aikido pada penggalian prinsip, konsep, dan falsafah Aiki dalam setiap teknik yang dipelajari, serta bertujuan untuk memelihara dan menghidupkan falsafah dan sikap hidup O Sensei Morihei Ueshiba (Pendiri Aikido) dalam latihan dan hidup sehari-hari.
         </p>
-        <p>
           <Link
             href="https://blog.tenkeiaikidojo.org/2013/07/sejarah-tenkei-aikidojo.html"
           >
             Baca sejarah lengkapnya di sini
           </Link>
-        </p>
+
 
         
       </main>
