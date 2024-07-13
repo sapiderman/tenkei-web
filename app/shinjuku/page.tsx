de4e1521397a86d9d08e403016d85d14ccc5767a
budhi <sapiderman.lumpat@gmail.com>
2024-07-13 07:14:19 +0700

    formatting

--- app/shinjuku/page.tsx
+++ app/shinjuku/page.tsx
@@ -9,11 +9,14 @@ export default function Dojo() {
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1 className="mb-1 text-5xl font-bold"> About Shinjuku Aikikai </h1>
         <p className="text-justify">
-Tenkei Aikidojo is affiliated with Shinjuku Aikikai. Shinjuku Aikikai is located in the Shinjuku area of Tokyo, Japan. Shinjuku Aikikai is a member of the Aikikai Foundation, the parent organization of Aikido. Shinjuku Aikikai offers classes for adults and children.  The
-         
+          Tenkei Aikidojo is affiliated with Shinjuku Aikikai. Shinjuku Aikikai
+          is located in the Shinjuku area of Tokyo, Japan. Shinjuku Aikikai is a
+          member of the Aikikai Foundation, the parent organization of Aikido.
+          Shinjuku Aikikai offers classes for adults and children. The
         </p>
         <Link href="https://www.shinjukuaikikai.com/">
-          To learn more about Shinjuku Aikika, click here https://www.shinjukuaikikai.com/
+          To learn more about Shinjuku Aikika, click here
+          https://www.shinjukuaikikai.com/
         </Link>
       </main>
       <Footer />
