dec6e7611df0f7131866a5bd723fc6503108a36f
budhi <sapiderman.lumpat@gmail.com>
2024-11-27 09:54:03 +0700

    update to light theme

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,8 +10,8 @@ export default function Dojo() {
         <h1 className="mb-1 text-5xl font-bold"> About Tenkei Aikidojo </h1>
         <p className="text-justify">
           Tenkei Aikidojo is a group of dojos under the guidance of Sensei Eka
-          Machdi Ramdani (4th Dan), which is the Indonesian branch of Shinjuku Aikikai
-          Dojo. Tenkei dojo emphasizes Aikido training by exploring the
+          Machdi Ramdani (4th Dan), which is the Indonesian branch of Shinjuku
+          Aikikai Dojo. Tenkei dojo emphasizes Aikido training by exploring the
           principles, concepts, and philosophies of Aiki in every techniques
           learned, and aim to maintain and continue the philosophies and
           attitudse of O Sensei Morihei Ueshiba (Founder of Aikido) in daily
--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4">
             Welcome to the new Tenkei web&nbsp;
           </p>
-          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:h-auto lg:w-auto lg:bg-none">
             <a
               className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
               href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -21,7 +21,6 @@ export default function Home() {
               <Image
                 src="/tenkei_logo.png"
                 alt="Tenkei Logo"
-                className="dark"
                 width={200}
                 height={200}
                 priority
@@ -30,9 +29,9 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:lg:h-[360px] z-[-1]">
           <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            className="relative"
             src="/tenkei_text_logo.png"
             alt="Tenkei text banner"
             width={1000}
@@ -44,7 +43,7 @@ export default function Home() {
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
           <a
             href="/about"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -61,7 +60,7 @@ export default function Home() {
 
           <Link
             href="/dojos"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -78,7 +77,7 @@ export default function Home() {
 
           <a
             href="https://blog.tenkeiaikidojo.org/"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -95,7 +94,7 @@ export default function Home() {
 
           <a
             href="/shinjuku"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100"
             target="_blank"
             rel="noopener noreferrer"
           >
