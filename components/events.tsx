e99f685251561670cc77d736aa64cc9a437405a0
budhi <sapiderman.lumpat@gmail.com>
2025-01-21 08:08:34 +0700

    formatting with linter

--- components/events.tsx
+++ components/events.tsx
@@ -4,10 +4,7 @@ import Link from "next/link";
 export default function Events() {
   return (
     <div>
-      <h2 className="flex justify-center mb-5 text-5xl font-bold">
-        Events
-        {" "}
-        </h2>
+      <h2 className="flex justify-center mb-5 text-5xl font-bold">Events </h2>
       <div className="flex justify-center">
         <Image
           className="seminar_image"
