7185bce9f557551a060ae0e983e5a4af92eeafaa
budhi <sapiderman.lumpat@gmail.com>
2025-03-01 17:25:13 +0700

    1. update faveicon 2. add join now button 3. Add joinbutton component

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,8 +4,7 @@ export default function Footer() {
     <footer style={{ backgroundColor: "#363636" }}>
       <div className="mx-auto max-w-7xl overflow-hidden py-5 px-6 lg:px-8">
         <p className="text-xs sm:text-center leading-5 text-gray-400">
-          Made with 💖 &copy;2024 Tenkei Aikidojo. All rights reserved. Version:
-          {pkg.version}.
+          Made with 💖 &copy;2024 Tenkei Aikidojo. All rights reserved. Version:{ pkg.version }.
           <a
             href="mailto:info@tenkeiaikidojo.org"
             className="text-blue-500 hover:underline ml-4"
