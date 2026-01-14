277c8b6227c68d45cb3dd2bb3d68afa3955190aa
budhi <budhiw@protonmail.com>
2026-01-14 19:28:39 +0700

    update join button to jump to register

--- components/joinButton.tsx
+++ components/joinButton.tsx
@@ -3,8 +3,8 @@
 export default function JoinButton() {
   return (
     <button
-      onClick={() => (window.location.href = "mailto:info@tenkeiaikidojo.org")}
-      className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+      onClick={() => (window.location.href = "https://rei.tenkeiaikidojo.org/v1/register")}
+      className="mt-4 px-4 py-2 bg-blue-700 text-white rounded hover:bg-blue-700"
     >
       Join Now
     </button>
