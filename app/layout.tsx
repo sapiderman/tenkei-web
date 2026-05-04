07ff78a16de04274071d59f6b50658f3b04aa2b3
Budhi <964106+sapiderman@users.noreply.github.com>
2026-05-04 18:04:42 +0700

    Fix anaytics (#79)
    bad imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,9 +39,6 @@ export const metadata: Metadata = {
   },
 };
 
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
 export default function RootLayout({
   children,
 }: Readonly<{
